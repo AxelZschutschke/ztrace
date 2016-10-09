a91e4d423944e46eb16b70a19197e811740ba3e7
Axel Zschutschke <axel@zschutschke.de>
2016-10-09 08:08:08 +0200

    added Types - common header file of type definitions

---
+++ src/Traceable.h
@@ -0,0 +1,15 @@
+//
+// Created by Axel Zschutschke on 10/9/16.
+//
+
+#ifndef ZTRACER_TRACEABLE_H
+#define ZTRACER_TRACEABLE_H
+
+namespace ztrace {
+    
+    class Hittable {
+
+    };
+}
+
+#endif //ZTRACER_TRACEABLE_H
---
+++ src/Types.h
@@ -0,0 +1,21 @@
+//
+// Created by Axel Zschutschke on 10/8/16.
+//
+
+#ifndef ZTRACER_TYPES_H
+#define ZTRACER_TYPES_H
+
+#include <string>
+
+namespace ztrace
+{
+    using Real = double;
+    using Int  = long;
+    using Size = size_t;
+    using String = std::string;
+
+    template<typename Target, typename Source>
+    Target convert( Source const & s );
+}
+
+#endif //ZTRACER_TYPES_H
