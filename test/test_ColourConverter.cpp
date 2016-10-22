f86783fe0629fa8bc6ca20c92cd55ae5ae2df312
Axel Zschutschke <axel.zschutschke@iec.tu-freiberg.de>
2016-10-22 22:30:51 +0200

    [test] ColourConverter now tested more extesively

--- test/test_ColourConverter.cpp
+++ test/test_ColourConverter.cpp
@@ -16,13 +16,65 @@ using ztrace::Real;
 template <Int colourRange>
 using ColourConverter = ztrace::ColourConverter<colourRange>;
 
-TEST(Colour_test, constructor_real)
+TEST(Colour_test, constructor_Real_Gray)
+{
+    ColourConverter<128> test{0.25};
+    EXPECT_EQ(test.red(), 32);
+    EXPECT_EQ(test.green(), 32);
+    EXPECT_EQ(test.blue(), 32);
+}
+
+TEST(Colour_test, constructor_Real)
+{
+    ColourConverter<128> test{(Real)0.25,(Real)0.5,(Real)-0.2};
+    EXPECT_EQ(test.red(), 32);
+    EXPECT_EQ(test.green(), 64);
+    EXPECT_EQ(test.blue(), 0);
+}
+
+TEST(Colour_test, constructor_Int)
+{
+    ColourConverter<256> test{(Int)128,(Int)256,(Int)32};
+    EXPECT_EQ(test.red(), 128);
+    EXPECT_EQ(test.green(), 256);
+    EXPECT_EQ(test.blue(), 32);
+}
+
+TEST(Colour_test, constructor_Vector )
+{
+    ztrace::Vector colourVector{0.5,-0.5,1.5};
+    ColourConverter<128> test{colourVector};
+    EXPECT_EQ(test.red(), 64);
+    EXPECT_EQ(test.green(), 0);
+    EXPECT_EQ(test.blue(), 128);
+}
+
+TEST(Colour_test, setRGB_Int )
+{
+    ColourConverter<128> test{};
+    test.setRGB((Int) 10, (Int) 200, (Int) -10 );
+    EXPECT_EQ(test.red(), 10);
+    EXPECT_EQ(test.green(), 128);
+    EXPECT_EQ(test.blue(), 0);
+}
+
+TEST(Colour_test, setRGB_Real )
 {
     ColourConverter<128> test{};
     test.setRGB((Real)0.5, (Real)1.5, (Real)-0.5);
     EXPECT_EQ(test.red(), 64);
     EXPECT_EQ(test.green(), 128);
     EXPECT_EQ(test.blue(), 0);
+}
+
+TEST(Colour_test, setRGB_Vector )
+{
+    ztrace::Vector colourVector{0.5,-0.5,1.5};
+    ColourConverter<128> test{};
+    test.setRGB(colourVector );
+    EXPECT_EQ(test.red(), 64);
+    EXPECT_EQ(test.green(), 0);
+    EXPECT_EQ(test.blue(), 128);
 }
 
 TEST(Colour_test, toStdout)
